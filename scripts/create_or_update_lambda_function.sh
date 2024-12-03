86f65681e450dc6e652dc875a55f972fb6835694
Milind Rastogi <milindrastogicloud@gmail.com>
2024-12-03 13:27:56 +0530

    updated code

--- scripts/create_or_update_lambda_function.sh
+++ scripts/create_or_update_lambda_function.sh
@@ -1,11 +1,11 @@
 echo "This is the image tag to be used: $SHA"
-if aws lambda get-function --function-name fastapi_app; then
+if aws lambda get-function --function-name fastapi_app1; then
   echo "Updating Lambda function code..."
   aws lambda update-function-code --function-name fastapi_app \
     --image-uri 637423521355.dkr.ecr.ap-south-1.amazonaws.com/milind/fastapi:$SHA
 else
   echo "Creating new Lambda function 'fastapi_app'..."
-  aws lambda create-function --function-name fastapi_app \
+  aws lambda create-function --function-name fastapi_app1 \
     --package-type Image \
     --code ImageUri=637423521355.dkr.ecr.ap-south-1.amazonaws.com/milind/fastapi:$SHA \
     --role arn:aws:iam::637423521355:role/lambda-execution-role \
